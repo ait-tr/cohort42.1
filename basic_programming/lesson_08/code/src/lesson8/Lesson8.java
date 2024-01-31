e185afcc8ac54e9d5c3199ff9a0d3dc0c94c2057
Sergey Iryupin <biblelamp@yandex.ru>
2024-01-31 10:12:40 +0100

    java_basic lesson #8 init

---
+++ basic_programming/lesson_08/code/src/lesson8/Lesson8.java
@@ -0,0 +1,12 @@
+package lesson8;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #8
+ *
+ * @version 31-Jan-24
+ */
+public class Lesson8 {
+    public static void main(String[] args) {
+
+    }
+}
