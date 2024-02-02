8c3512be1eca946457994a491847f04e6df93fe1
Sergey Iryupin <biblelamp@yandex.ru>
2024-02-02 13:06:25 +0100

    java_basic lesson #9 init

---
+++ basic_programming/lesson_09/code/src/lesson9/Lesson9.java
@@ -0,0 +1,11 @@
+package lesson9;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #9
+ *
+ * @version 2-Feb-24
+ */
+public class Lesson9 {
+    public static void main(String[] args) {
+    }
+}
