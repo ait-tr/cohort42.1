5cb5be80c7910e281466f047ce814a35a2363ba9
Sergey Iryupin <biblelamp@yandex.ru>
2024-02-04 12:33:00 +0100

    java_basic lesson #10 init

---
+++ basic_programming/lesson_10/code/src/lesson10/lesson10.java
@@ -0,0 +1,11 @@
+package lesson10;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #10
+ *
+ * @version 5-Feb-24
+ */
+public class Lesson10 {
+    public static void main(String[] args) {
+    }
+}
