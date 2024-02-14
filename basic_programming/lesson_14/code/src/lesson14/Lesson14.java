db272db427002e043c5a3a00a30998bb5ea1c0d7
Sergey Iryupin <biblelamp@gmail.com>
2024-02-14 11:35:37 +0100

    java_basic lesson #14 init

---
+++ basic_programming/lesson_14/code/src/lesson14/Lesson14.java
@@ -0,0 +1,11 @@
+package lesson14;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #14
+ *
+ * @version 14-Feb-24
+ */
+public class Lesson14 {
+    public static void main(String[] args) {
+    }
+}
