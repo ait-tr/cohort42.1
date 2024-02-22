056e98e3c4a448ed1c172b416bc0d6fc13f1293f
Sergey Iryupin <biblelamp@gmail.com>
2024-02-22 07:57:36 +0100

    java_basic lesson #17 update

---
+++ basic_programming/lesson_17/code/src/lesson17/Cat.java
@@ -0,0 +1,27 @@
+package lesson17;
+
+public class Cat {
+    private String name;
+    String color;
+    int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        if (name != null) {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void voice() {
+        System.out.println("Meow!");
+    }
+}
--- basic_programming/lesson_17/code/src/lesson17/Lesson17.java
+++ basic_programming/lesson_17/code/src/lesson17/Lesson17.java
@@ -7,5 +7,9 @@ package lesson17;
  */
 public class Lesson17 {
     public static void main(String[] args) {
+        Cat cat = new Cat("Murzik", "white", 3);
+        cat.setName(null);
+        System.out.println("Name: " + cat.getName() + ", color: " + cat.color + ", age: " + cat.age);
+        cat.voice();
     }
 }
