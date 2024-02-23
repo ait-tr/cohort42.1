f76087e5accbe640009b1fed28d893c882ffb8e3
Sergey Iryupin <biblelamp@gmail.com>
2024-02-23 11:07:17 +0100

    java_basic lesson #18 init

---
+++ basic_programming/lesson_18/code/src/lesson18/Lesson18.java
@@ -0,0 +1,11 @@
+package lesson18;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #18
+ *
+ * @version 23-Feb-24
+ */
+public class Lesson18 {
+    public static void main(String[] args) {
+    }
+}
