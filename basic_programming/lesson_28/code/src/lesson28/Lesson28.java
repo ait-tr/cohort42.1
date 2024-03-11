db0523d415b859fc3e19ae061725a29dc5c9d3d1
Sergey Iryupin <biblelamp@gmail.com>
2024-03-11 08:18:10 +0100

    java_basic lesson #28 init

---
+++ basic_programming/lesson_28/code/src/lesson28/Lesson28.java
@@ -0,0 +1,11 @@
+package lesson28;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #28
+ *
+ * @version 18-Mar-24
+ */
+public class Lesson28 {
+    public static void main(String[] args) {
+    }
+}
