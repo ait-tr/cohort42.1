b20a7ac0b4adbdbe25521f0fd1011b6622fd92e7
Sergey Iryupin <biblelamp@gmail.com>
2024-03-16 10:53:35 +0100

    java_basic lesson #27 update

--- basic_programming/lesson_27/code/src/lesson27/Lesson27.java
+++ basic_programming/lesson_27/code/src/lesson27/Lesson27.java
@@ -7,5 +7,12 @@ package lesson27;
  */
 public class Lesson27 {
     public static void main(String[] args) {
+        // #1
+        Figure[] figures = {new Circle(5), new Rectangle(3, 4), new Triangle(4, 5, 3)};
+        for (Figure figure : figures) {
+            System.out.println(figure.getClass().getSimpleName());
+            System.out.println(figure.calcArea());
+            System.out.println(figure.calcPerimeter());
+        }
     }
 }
