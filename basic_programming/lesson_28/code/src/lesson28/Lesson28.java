4bf774f458122b115f83bfb13dcb220aefc57183
Sergey Iryupin <biblelamp@gmail.com>
2024-03-19 15:04:42 +0100

    java_basic lesson #28 update

--- basic_programming/lesson_28/code/src/lesson28/Lesson28.java
+++ basic_programming/lesson_28/code/src/lesson28/Lesson28.java
@@ -7,5 +7,13 @@ package lesson28;
  */
 public class Lesson28 {
     public static void main(String[] args) {
+        OuterAndInnerClass outerAndInnerClass = new OuterAndInnerClass();
+        outerAndInnerClass.useInnerClass();
+
+        OuterAndInnerStaticClass outerAndInnerStaticClass = new OuterAndInnerStaticClass();
+        outerAndInnerStaticClass.useInnerClass();
+
+        //OuterAndInnerClass.InnerClass innerClass = new OuterAndInnerClass.InnerClass();
+        OuterAndInnerStaticClass.InnerClass innerStaticClass = new OuterAndInnerStaticClass.InnerClass();
     }
 }
