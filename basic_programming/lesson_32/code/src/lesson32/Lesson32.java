38e499789e24eea31a4bf08965059a2475de2824
Sergey Iryupin <biblelamp@gmail.com>
2024-03-27 10:38:09 +0100

    java_basic lesson #32 init

---
+++ basic_programming/lesson_32/code/src/lesson32/Lesson32.java
@@ -0,0 +1,11 @@
+package lesson32;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #32
+ *
+ * @version 27-Mar-24
+ */
+public class Lesson32 {
+    public static void main(String[] args) {
+    }
+}
