f9b0059d2c20819284ea16683369131bc0b56b98
Sergey Iryupin <biblelamp@gmail.com>
2024-04-10 16:14:20 +0200

    java_basic lesson #36 init

---
+++ basic_programming/lesson_36/code/src/lesson36/Lesson36.java
@@ -0,0 +1,11 @@
+package lesson36;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #36
+ *
+ * @version 10-Apr-24
+ */
+public class Lesson36 {
+    public static void main(String[] args) {
+    }
+}
