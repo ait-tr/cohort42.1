3f39433cf42d9e2f5792ddb9cf7156086421354f
Sergey Iryupin <biblelamp@gmail.com>
2024-04-12 12:41:08 +0200

    java_basic lesson #37 init

---
+++ basic_programming/lesson_37/code/src/lesson37/Lesson37.java
@@ -0,0 +1,11 @@
+package lesson37;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #37
+ *
+ * @version 12-Apr-24
+ */
+public class Lesson37 {
+    public static void main(String[] args) {
+    }
+}
