894ef2507c137e12bf6ab3e7c9ef0114cd8a2acb
Sergey Iryupin <biblelamp@gmail.com>
2024-04-28 11:51:08 +0200

    java_basic lesson #44 init

---
+++ basic_programming/lesson_44/code/src/lesson44/Lesson44.java
@@ -0,0 +1,11 @@
+package lesson44;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic, Lesson #44
+ *
+ * @version 29-Apr-24
+ */
+public class Lesson43 {
+    public static void main(String[] args) {
+    }
+}
