abfa393538aef0cef3f471461648c3d2b683b90c
Sergey Iryupin <biblelamp@gmail.com>
2024-05-04 09:47:31 +0200

    java_basic lesson #45 update

--- basic_programming/lesson_45/code/src/lesson45/Lesson45.java
+++ basic_programming/lesson_45/code/src/lesson45/Lesson45.java
@@ -1,5 +1,10 @@
 package lesson45;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+
 /**
  * AIT-TR, cohort 42.1, Java Basic, Lesson #45
  *
@@ -7,5 +12,23 @@ package lesson45;
  */
 public class Lesson45 {
     public static void main(String[] args) {
+        LocalDate dateNow = LocalDate.now();
+        LocalTime timeNow = LocalTime.now();
+        LocalDateTime dateTimeNow = LocalDateTime.now();
+        System.out.println(dateNow);
+        System.out.println(timeNow);
+        System.out.println(dateTimeNow);
+
+        LocalDate okudzava = LocalDate.of(1924, 5, 9);
+        System.out.println(okudzava);
+        System.out.println(okudzava.equals(LocalDate.of(1924, 5, 9)));
+        System.out.println(timeNow.plusHours(3));
+        System.out.println(dateNow.plusWeeks(1));
+        System.out.println(dateNow.minusYears(1));
+        System.out.println(dateNow.plusYears(1));
+
+        Period period = Period.between(okudzava, dateNow);
+        System.out.println(period);
+        System.out.println(period.getYears());
     }
 }
