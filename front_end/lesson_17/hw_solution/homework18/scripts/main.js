c3346f822dc125795af47e45cd0d6dfe52a07ebf
Rodion Shilinski <Shilinski.Rodion@outlook.com>
2024-06-28 12:55:26 +0000

    (HW)-user: 'Rodion_Shilinski-Shilinski.Rodion@outlook.com', commit time: '2024-06-28 12:55:26'

---
+++ front_end/lesson_17/hw_solution/homework18/scripts/main.js
@@ -0,0 +1,25 @@
+fetch('https://jsonplaceholder.typicode.com/todos')
+    .then(response => response.json())
+    .then(data => {
+    
+    const slicedData = data.slice(10, 15);
+
+    const formattedData = slicedData.map((item, index) => ({
+        id: item.id,
+        index: index + 1,
+        title: item.title,
+        completed: item.completed
+    }));
+
+    const todoContainer = document.getElementById('todo-container');
+
+            formattedData.forEach(item => {
+                const todoItem = document.createElement('div');
+                todoItem.className = `todo-item ${item.completed ? 'completed' : 'not-completed'}`;
+                todoItem.innerHTML = `<strong>${item.index}.</strong> ${item.title} (ID: ${item.originalId})`;
+                todoContainer.appendChild(todoItem);
+})})
+
+.catch(error => {
+    console.error('Ошибка при загрузке данных:', error);
+});
