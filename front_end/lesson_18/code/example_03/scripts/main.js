35f3c7cb23010fa3c8cca4ff448ee3bf6b493c34
Vitaliy Vynnyk <Vitaliy.vinnik@gmail.com>
2024-07-04 20:45:37 +0200

    improves lesson 18

--- front_end/lesson_18/code/example_03/scripts/main.js
+++ front_end/lesson_18/code/example_03/scripts/main.js
@@ -1,23 +1,37 @@
 async function example() {
-    // console.log("I am func example");
-    return 0;
+  // console.log("I am func example");
+  return 0;
 }
-const example02 = async () => {
-}
+const example02 = async () => {};
 // example().then(res => {console.log(res)});
 
 const gitHubFetch = async () => {
+  // const response = await fetch('https://api.github.com/users/facebook');
+  // const responseForRepos = await fetch('https://api.github.com/users/facebook/repos');
+  // const reposOfUser = await responseForRepos.json();
+  // const bodyData = await response.json();
+  // console.log(bodyData);
+  // console.log(reposOfUser);
+  // return bodyData.avatar_url;
+  try {
     const response = await fetch('https://api.github.com/users/facebook');
-    const responseForRepos = await fetch('https://api.github.com/users/facebook/repos');
+    const responseForRepos = await fetch(
+      'https://api.github.com/users/facebook/repos'
+    );
     const reposOfUser = await responseForRepos.json();
     const bodyData = await response.json();
     console.log(bodyData);
     console.log(reposOfUser);
     return bodyData.avatar_url;
-}
+  } catch(error) {
+    console.log('Ошибка' + error)
+  } finally {
+    console.log()
+  }
+};
 
-gitHubFetch().then((data) => {console.log(data + "   ===   опять")});
+gitHubFetch().then((data) => {
+  console.log(data + '   ===   опять');
+});
 
 console.log('main code');
-
-
